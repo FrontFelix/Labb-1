397995f5adfbe56239623709ce958943c97f9ade
felixloot <felix.bakkum2002@gmail.com>
2021-11-16 18:15:11 +0100

    Variabels + Readme

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// Global Variabels
+const fighterImage = document.getElementById('fighter')
 const pages = {
     "starter": document.getElementById('start'),
     "settings": document.getElementById('settings'),
     "fightscene": document.getElementById('fightingScene')
 }
-
 const player =
 {
     "name": "default",
@@ -12,33 +11,26 @@ const player =
     "weapon": "pistol",
     "age": 13
 }
+const arena = {
+    "wolf": document.getElementById('wolf'),
+    "person": document.getElementById('fighterArena'),
+    "image": document.getElementById('fighterArena'),
+    "name": document.getElementById('arenaName'),
+    "age": document.getElementById('arenaAge'),
+    "weapon": document.getElementById('arenaWeapon'),
+    "attackBtn": document.getElementById('buttonAttack'),
+    "friendBtn": document.getElementById('buttonFriend'),
+    "weaponBullet": document.getElementById('bullet'),
+    "weaponSwing": document.getElementById('swing'),
+    "weaponScratch": document.getElementById('buttonFriend')
+}
 
-const fighterImage = document.getElementById('fighter')
-// Global Variabels
-
-
-// Arena 
-const wolf = document.getElementById('wolf')
-const person = document.getElementById('fighterArena')
-const arenaImage = document.getElementById('fighterArena')
-const arenaName = document.getElementById('arenaName')
-const arenaAge = document.getElementById('arenaAge')
-const arenaWeapon = document.getElementById('arenaWeapon')
-
-// Arena buttons
-const attackBtn = document.getElementById('buttonAttack')
-const friendBtn = document.getElementById('buttonFriend')
-
-// Arena Weapons
-const bullet = document.getElementById('bullet')
-const swing = document.getElementById('swing')
-const scratch = document.getElementById('scratch')
 
 function updateFighter(item) {
-    attackBtn.style.display = "block"
-    friendBtn.style.display = "block"
-    bullet.style.display = "none"
-    swing.style.display = "none"
+    arena.attackBtn.style.display = "block"
+    arena.friendBtn.style.display = "block"
+    arena.weaponBullet.style.display = "none"
+    arena.weaponSwing.style.display = "none"
     if(item.id === "Male") {
         player.fighter = "./images/fighter1.png"
         player.weapon = "Sword"
@@ -59,28 +51,28 @@ function settingsFighter() {
     player.name = nameValue
     pages.settings.style.display = "none"
     pages.fightscene.style.display = "block"
-    arenaImage.src = player.fighter
-    arenaName.innerHTML = player.name
-    arenaAge.innerHTML = player.age
-    arenaWeapon.innerHTML = player.weapon
+    arena.image.src = player.fighter
+    arena.name.innerHTML = player.name
+    arena.age.innerHTML = player.age
+    arena.weapon.innerHTML = player.weapon
     console.log(player)
 
 }
 
 
 function fightWolf() {
-    attackBtn.style.display = "none"
-    friendBtn.style.display = "none"
+    arena.attackBtn.style.display = "none"
+    arena.friendBtn.style.display = "none"
     if(player.weapon === "Pistol") {
-        bullet.style.display = "block"
-        bullet.style.animation = "bulletShoot 1s linear"
-        setTimeout(function(){wolf.style.animation = "rotatePerson 1s linear"}, 4000)
-        wolf.style.animation = "rotateWolf 1s linear"
+        arena.weaponBullet.style.display = "block"
+        arena.weaponBullet.style.animation = "bulletShoot 1s linear"
+        setTimeout(function(){arena.wolf.style.animation = "rotatePerson 1s linear"}, 4000)
+        arena.wolf.style.animation = "rotateWolf 1s linear"
         setTimeout(function(){alert("YOU WON THE GAME BY KILLING THE WOLF")}, 3000)
     }else if(player.weapon === "Sword") {
-        swing.style.display = "block"
-        swing.style.animation = "rotateSwing 1s linear"
-        setTimeout(function(){wolf.style.animation = "rotateWolf 1s linear"}, 1000)
+        arena.weaponSwing.style.display = "block"
+        arena.weaponSwing.style.animation = "rotateSwing 1s linear"
+        setTimeout(function(){arena.wolf.style.animation = "rotateWolf 1s linear"}, 1000)
         setTimeout(function(){alert("YOU WON THE GAME BY KILLING THE WOLF")}, 3000)
     }
 
@@ -92,10 +84,10 @@ function fightWolf() {
 }
 
 function friendWolf() {
-    attackBtn.style.display = "none"
-    friendBtn.style.display = "none"
-    scratch.style.display = "block"
-    scratch.style.animation = "rotateScratch 1s linear"
+    arena.attackBtn.style.display = "none"
+    arena.friendBtn.style.display = "none"
+    arena.weaponScratch.style.display = "block"
+    arena.weaponScratch.style.animation = "rotateScratch 1s linear"
     setTimeout(function(){alert("THE WOLF KILLED YOU, YOU LOST!")}, 3000)
 
     setTimeout(function(){
--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
+##FIGHTING GAME
+
+Du kan välja HERO, och sen slåss mot ett STORT MONSTER!!!!
+
+![Preview](./images/preview.png "Preview")
 Här är min [Demo Sida](https://frontfelix.github.io/Labb-1/).
 
